d2b84c1bb186503aaf3e82bc463c683d89898b6f
Alexey Kotcov <richyshark@gmail.com>
2020-04-10 21:10:49 +0300

    asd

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ div
         a.buts_text Игры
     .lk
       .user
-        img(src="../assets/img/Header/Ава.png")
+        img(src="../assets/img/Header/Оформление авы.png")
         .user_info
           a(style="font-weight:300;") Aska.again
           a(style="font-weight:600;") 276.76 ₽
@@ -27,15 +27,15 @@ div
   .footer
     .footer_line
     .panel
-      .leftside
+      ul.panel_left
         img(src="../assets/img/LOGO.png" height=70 style="    filter: alpha(Opacity=50);opacity: 0.5; ")
-        .leftside_item
+        li.panel_left
           a FAQ
-        .leftside_item
+        li.panel_left
           a Контакты
-        .leftside_item
+        li.panel_left
           a Пользовательское соглашение
-      .rightside
+      .panel_right
         a tech_mail@mail.com
 </template>
 
@@ -47,53 +47,37 @@ export default {
 </script>
 
 <style>
-.footer{
-  height: 86px;
-  background: radial-gradient(50% 50% at 50% 100%, rgba(88, 9, 199, 0.86) 0%, rgb(75, 3, 168) 100%);
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  flex-direction: column;
-}
-.footer_line{
-  position: relative;
-  top:0;
-  height: 7px;
-  width: 100%;
-  background: #FFBC00;
-}
 .panel{
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
 }
-.leftside{
+.panel ul{
   display: flex;
-  align-items: center;
-  justify-content: flex-start;
   flex-direction: row;
-  margin-left: 19px;
+  align-items: center;
+  margin-left: 19px; /* Отступ слева в браузере IE и Opera */
+  padding-left: 0; /* Отступ слева в браузере Firefox, Safari, Chrome */
+}
+.panel li{
+  list-style-type: none;
+  margin-left: 47px;
   font-family: Proxima Nova;
   font-style: normal;
   font-weight: 600;
   font-size: 16px;
   line-height: 19px;
   color: #FFFFFF;
-  padding: 6px;
 }
-.leftside_item{
-  display: flex;
-  margin-left: 47px;
-}
-.rightside{
-  font: 14px/16px arial, helvetica, sans-serif;
+.panel_right{
+  font-family: Proxima Nova;
   font-style: normal;
   font-weight: 600;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: right;
   color: #FFFFFF;
-  padding: 6px;
   margin-right: 55px;
 }
 .buts ul{
@@ -101,6 +85,43 @@ export default {
   flex-direction: row;
   justify-content: space-between;
 }
+
+.header {
+  height: 86px;
+  background: radial-gradient(50% 50% at 50% 100%, rgba(88, 9, 199, 0.86) 0%, rgb(75, 3, 168) 100%);
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.buts{
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  padding-left: 0;
+  margin-left: 19px;
+}
+
+.buts li {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  font-family: Proxima Nova;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 19px;
+  color: #FFFFFF;
+  padding: 17px;
+}
+
+.buts_text{
+  margin: 0 6px;
+}
+
 .lk{
   display: flex;
   align-items: center;
@@ -144,38 +165,23 @@ export default {
   color: #FFFFFF;
   margin-right: 25px;
 }
-.header {
+
+.footer{
   height: 86px;
   background: radial-gradient(50% 50% at 50% 100%, rgba(88, 9, 199, 0.86) 0%, rgb(75, 3, 168) 100%);
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  flex-direction: column;
+}
+.footer_line{
   position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-
-.buts{
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  padding-left: 0;
-  margin-left: 19px;
-}
-.buts li {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-direction: row;
-  font-family: Proxima Nova;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 19px;
-  color: #FFFFFF;
-  padding: 17px;
-}
-.buts_text{
-  margin: 0 6px;
+  top:0;
+  height: 7px;
+  width: 100%;
+  background: #FFBC00;
 }
 
 @font-face {
